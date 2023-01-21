72536a27879ea2247359d4a969aa36dd8b9e4d57
Simon Vareille <si.vareille@gmail.com>
2023-01-21 14:28:05 +0100

    FIx call of gpgconf for homedir

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,7 @@ use std::{path::PathBuf, process::Command};
 use anyhow::{Result, Context};
 
 pub fn get_homedir() -> Result<PathBuf> {
-    let output = Command::new("gpgconf1")
+    let output = Command::new("gpgconf")
         .args(["--list-dirs", "homedir"])
         .output().context("Call to gpgconf failed")?;
     Ok(PathBuf::from(String::from_utf8(output.stdout)?.trim()))
